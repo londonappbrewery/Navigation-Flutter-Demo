500582e4914ce811651cc0246e4b82047a16be15
Angela <8798027+angelabauer@users.noreply.github.com>
2019-04-02 17:05:24 +0100

    Update screen 0

---
+++ lib/screen0.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Screen0 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text('Screen 0'),
+      ),
+      body: Column(
+        children: <Widget>[
+          RaisedButton(
+            color: Colors.red,
+            child: Text('Go To Screen 1'),
+            onPressed: () {
+              //Navigate to Screen 1
+            },
+          ),
+          RaisedButton(
+            color: Colors.blue,
+            child: Text('Go To Screen 2'),
+            onPressed: () {
+              //Navigate to Screen 2
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
